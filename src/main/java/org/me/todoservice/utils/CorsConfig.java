124c14bea517729778b930d15f99b65685b748bb
james <colorlife@163.com>
2020-03-23 21:59:34 +0800

    允许跨域访问

---
+++ src/main/java/org/me/todoservice/utils/CorsConfig.java
@@ -0,0 +1,31 @@
+package org.me.todoservice.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * CORS 跨域访问配置
+ */
+@Configuration
+public class CorsConfig {
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*"); // 允许任何域名
+		corsConfiguration.addAllowedHeader("*"); // 允许任何头
+		corsConfiguration.addAllowedMethod("GET");
+		corsConfiguration.addAllowedMethod("POST");
+		corsConfiguration.addAllowedMethod("PUT");
+		corsConfiguration.addAllowedMethod("DELETE");
+		return corsConfiguration;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig()); // 注册
+		return new CorsFilter(source);
+	}
+}
